c5904917443e97c05142fbac31ae3ce895b1814d
Lucrecia <Lucrecia@cpt-co-265A01.ADS.CPUT.AC.ZA>
2014-05-21 07:10:44 +0200

    AssignmntInProgressCommit

---
+++ src/test/java/com/mycompany/test/repository/AgentRepositoryTest.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.test.repository;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Lucrecia
+ */
+public class AgentRepositoryTest {
+    
+    public AgentRepositoryTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
